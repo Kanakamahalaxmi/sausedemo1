13b7005b3f59185377b6581fedb4c72e53e356fc
maha laxmi <mahalaxmikaranam@gmail.com>
2023-10-10 12:47:27 +0530

    second commit

---
+++ src/test/java/sausedemo/Sause.java
@@ -0,0 +1,50 @@
+package sausedemo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Sause {
+	 public static void main(String[] args) throws InterruptedException {
+	       
+	        System.setProperty("webdriver.chrome.driver",  "C:\\Users\\ss\\Downloads\\chromedriver-win64\\chromedriver.exe");
+
+	        
+	        ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--start-maximized"); 
+
+	        // Start ChromeDriver with ChromeOptions
+	        WebDriver positivedriver = new ChromeDriver(options);
+
+	        // Positive scenario
+	        positivedriver.get("https://www.saucedemo.com");
+	        login(positivedriver, "standard_user", "secret_sauce");
+	       // System.out.println("Positive Scenario: " + positivedriver.getCurrentUrl());
+	        
+	        Thread.sleep(9000);
+
+	        // Open a new Chrome 
+	        WebDriver negativeScenarioDriver = new ChromeDriver(options);
+	        negativeScenarioDriver.get("https://www.saucedemo.com");
+
+	        // Negative scenario
+	        login(negativeScenarioDriver, "vikul", "golang");
+	        //System.out.println("Negative Scenario: " + negativeScenarioDriver.getCurrentUrl());
+
+	        
+	    }
+
+	    private static void login(WebDriver driver, String username, String password) {
+	        WebElement usernameField = driver.findElement(By.id("user-name"));
+	        WebElement passwordField = driver.findElement(By.id("password"));
+	        WebElement loginButton = driver.findElement(By.id("login-button"));
+
+	        usernameField.sendKeys(username);
+	        passwordField.sendKeys(password);
+	        loginButton.click();
+	    }
+	}
+
+
